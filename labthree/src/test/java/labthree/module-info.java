58a6482ef9bc82db405aae504b783cf767f2d154
tom.sautereau@gmail.com <tom.sautereau@gmail.com>
2018-10-09 11:18:59 +0000

    Travis test

---
+++ labthree/src/test/java/labthree/module-info.java
@@ -0,0 +1,6 @@
+open module labthree {
+  requires org.junit.jupiter.api;
+  
+  // requires org.openjdk.jmh;  // JMH support
+  // requires org.openjdk.jmh.generator;
+}
---
+++ labthree/target/test/merged/labthree/module-info.java
@@ -0,0 +1,7 @@
+open
+module labthree {
+  requires org.junit.jupiter.api;
+  requires java.base;
+
+}
+
